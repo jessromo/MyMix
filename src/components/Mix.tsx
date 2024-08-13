cb3bd994dcd59b682506686d36f89e9ad3b029dd
Jessica Romo <jessiromo06@gmail.com>
2024-08-13 13:56:36 -0500

    fix netlify deploy

--- src/components/Mix.tsx
+++ src/components/Mix.tsx
@@ -3,7 +3,8 @@ import "../App.css";
 
 const clientSecret = "477ae9886eb54377a797884030593168";
 const clientId = "9d34b1f66cb14b4cbdf6f6ee27a35f12";
-const redirectUrl = "http://localhost:5173/callback";
+/* const redirectUrl = "http://localhost:5176/callback"; */
+const redirectUrl = "https://mymix.netlify.app/callback";
 
 const scope =
   "user-read-private user-read-email user-top-read user-follow-read user-library-read";
@@ -236,13 +237,12 @@ function Mix() {
             <div className="cd">
               <img src="cd.png" id="cdpic" />
             </div>
-          
           </div>
           <div className="logout">
-              <button id="logoutbutton" onClick={handleLogout}>
-                Log Out
-              </button>
-            </div>
+            <button id="logoutbutton" onClick={handleLogout}>
+              Log Out
+            </button>
+          </div>
         </div>
       )}
     </>
